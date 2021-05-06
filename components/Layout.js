c7a2f6d625767849861034aec41585ea3f8235d5
JDOG787 <64325143+JDOG787@users.noreply.github.com>
2021-05-05 22:26:40 -0700

    add metadata

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,32 @@
 import Head from "next/head";
 import React from "react";
+import image from "/meta-image.png";
 
 export default function Layout(props) {
     return (
         <div>
             <Head>
+                {/* Main meta tags */}
+                <meta name="description" content="A simple yet powerful bug tracker to help hunt down those bugs and eliminate them."/>
+                <meta name="keywords" content="bug bugs tracker jdog787 jdog bug-tracker bugtracker hunt bughunt bug-hunt"/>
+                <meta name="author" content="JDOG787"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <meta name="title" content="Bug Hunt"/>
+                <title>Bug Hunt</title>
 
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://bughunt.jdog.codes/"/>
+                <meta property="og:title" content="Bug Hunt"/>
+                <meta property="og:description" content="A simple yet powerful bug tracker to help hunt down those bugs and eliminate them."/>
+                <meta property="og:image" content={image}/>
+
+                {/* Twitter */}
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content="https://bughunt.jdog.codes/"/>
+                <meta property="twitter:title" content="Bug Hunt"/>
+                <meta property="twitter:description" content="A simple yet powerful bug tracker to help hunt down those bugs and eliminate them."/>
+                <meta property="twitter:image" content={image}/>
             </Head>
             {props.children}
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 import styles from '../styles/Home.module.css';
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
     return (
       <div>
-        <Image
-          src="/unknown.png"
-          alt="Picture of the author"
-          width={500}
-          height={500}
-        />
+  
+        <nav className={styles.landingNav}>
+          <div>
+            <Image
+                src="/logo.svg"
+                alt="logo"
+                width={200}
+                height={100}
+              />
+          </div>
+          <div>
+            <Link href="/"><a className={styles.landingNavLinks}>Home</a></Link>
+          </div>
+        </nav>
         <h1>Landing page</h1>
         <a href="/login">Login</a>
       </div>
