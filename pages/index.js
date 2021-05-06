4d5f7a11e44d84ecaf181e934855571f8b68f583
JDOG787 <redstoneexpert787@icloud.com>
2021-05-06 16:44:48 -0700

    hopefully meta works now

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,28 @@
 import styles from '../styles/Home.module.css';
 import Image from "next/image";
 import Link from "next/link";
+import Layout from "../components/Layout";
 
 export default function Home() {
     return (
-      <div>
-  
-        <nav className={styles.landingNav}>
-          <div>
-            <Image
-                src="/logo.svg"
-                alt="logo"
-                width={200}
-                height={100}
-              />
-          </div>
-          <div>
-            <Link href="/"><a className={styles.landingNavLinks}>Home</a></Link>
-          </div>
-        </nav>
-        <h1>Landing page</h1>
-        <a href="/login">Login</a>
-      </div>
+      <Layout>
+        <div>
+          <nav className={styles.landingNav}>
+            <div>
+              <Image
+                  src="/logo.svg"
+                  alt="logo"
+                  width={200}
+                  height={100}
+                />
+            </div>
+            <div>
+              <Link href="/"><a className={styles.landingNavLinks}>Home</a></Link>
+            </div>
+          </nav>
+          <h1>Landing page</h1>
+          <a href="/login">Login</a>
+        </div>
+      </Layout>
     )
 }
