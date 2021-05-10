62adb959b13828ed83d10c783dceecb251779639
JDOG787 <64325143+JDOG787@users.noreply.github.com>
2021-05-10 15:31:18 -0700

    feat: Make hero section on landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import styles from '../styles/Home.module.css';
 import Link from "next/link";
 import Layout from "../components/Layout";
-import InvertedBtn from '../components/inverted-btn';
+import InvertedBtn from '../components/Inverted-btn';
+import Btn from '../components/Btn';
 
 export default function Home() {
     return (
@@ -21,11 +22,19 @@ export default function Home() {
               <Link href="/"><a className={styles.landingNavLinks}>Features</a></Link>
             </div>
             <div className={styles.signupBtn}>
-              <InvertedBtn link="/login" text="Sign up"/>
+              <InvertedBtn link="/login" text="Login"/>
             </div>
           </nav>
-          <h1 className={styles.heroHeader}>Helping you elimiate them bugs</h1>
-          <a href="/login">Login</a>
+          <section className={styles.heroSection}>
+            <div>
+              <h1 className={styles.heroHeader}>Helping you <span className={styles.pop}>eliminate</span> them bugs</h1>
+              <p className={styles.heroInfo}>A Simple yet powerful bug and project tracker that aids you on accomplishing what you need to do.</p>
+              <Btn text="Get Started" link="/login"/>
+            </div>
+            <div>
+              <img className={styles.heroImage} src="/images/hunt-list.svg"/>
+            </div>
+          </section>
         </div>
       </Layout>
     )
